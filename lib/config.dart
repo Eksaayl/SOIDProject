f6738168ce7979546a9c07fead37681e925985f3
Eksaayl <phoneplays@yahoo.com>
2025-07-16 13:49:40 +0800

    Docker again

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Config {
-  static String get serverUrl => 'https://soidproject-production.up.railway.app';
+  static String get serverUrl => 'https://soidproject-50933881229.asia-east1.run.app';
 } 
