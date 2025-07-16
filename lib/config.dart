a5a5449ed21b836ee5c45e5a1cd0f92dbabb7525
Eksaayl <phoneplays@yahoo.com>
2025-07-16 15:30:41 +0800

    updated config

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Config {
-  static String get serverUrl => 'https://soidproject-50933881229.asia-east1.run.app';
+  static String get serverUrl => 'https://soidproject-221130199477.asia-east1.run.app';
 } 
